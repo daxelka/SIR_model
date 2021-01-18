559dc6a889a77fb5eccd6e801fa5aa2852f99cdf
daxelka <alina.dubovskaya@ul.ie>
2021-01-18 16:08:48 +0000

    simple SIR model

--- sir.py
+++ sir.py
@@ -4,10 +4,10 @@ from scipy.integrate import odeint
 
 class Sir_model:
     
-    def __init__(self, dt=0.01, T=10):
-        
-        self.dt = dt
-        self.T = T
+    def __init__(self, beta = 0.4, gamma = 0.04):
+                
+        self.beta = beta
+        self.gamma = gamma
         
     def equations(self, y, t):
         '''
@@ -16,25 +16,23 @@ class Sir_model:
         y(1) : stock of (I)nfected population
         y(2) : stock of (R)ecovered population  
         '''
-         
-        beta = 0.4
-#         gamma = 0.04
-#         N = sum(y)
+        S = y[0]
+        I = y[1]
+        R = y[2]
+        N = sum(y)
         
-#         dydt(0) = -beta/N *  np.multiply(I,S)
-#         dydt(1) = beta/N *  np.multiply(I,S) - gamma * I
-#         dydt(2) = gamma * I
-        dydt = -beta * y
+        dSdt = -self.beta / N * I * S
+        dIdt = self.beta / N * I * S - self.gamma * I
+        dRdt = self.gamma * I
+
+        dydt = [dSdt, dIdt, dRdt]
         return dydt
 
-    def integrate(self, y0):
-        '''Updates all states by integrating state of all nodes'''
-        t = np.linspace(0, self.T, int(self.T/self.dt))
+    def integrate(self, y0, t):
+        '''Integrate equations starting from initial conditions y0'''
         y = odeint(self.equations, y0, t)
         return y, t
 
-    def run(self):
-        
-#         y0 = np.array([10,1,0])
-        y0 = 5
-        return self.integrate(y0)
+    def run(self, y0 = [97, 3, 0],  dt = 0.01, T = 10):
+        t = np.linspace(0, T, int(T/dt))
+        return self.integrate(y0, t)
